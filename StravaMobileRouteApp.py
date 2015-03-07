fcefdae204184fbe526b9dc666d11fe0fbc530a8
Joemar Taganna <joemar.ct@gmail.com>
2015-03-07 10:42:08 +0800

    Improved strava interface

--- StravaMobileRouteApp.py
+++ StravaMobileRouteApp.py
@@ -3,14 +3,16 @@
 # 2) Save SMS data to Parse
 # 3) Create Strava route with Selenium 
 
-from flask import Flask, request
 import twilio.twiml
 import json,httplib,urllib
-import time
-from pyvirtualdisplay import Display
-from selenium import webdriver
+from strava import create_route
+from flask import Flask, request
 
 app = Flask(__name__)
+
+PARSE_APP_ID = "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K"
+PARSE_APP_KEY = "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq"
+
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
@@ -27,8 +29,8 @@ def hello():
 		     })})
 		connection.connect()
 		connection.request('GET', '/1/classes/Rider?%s' % params, '', {
-		       "X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-		       "X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq"
+		       "X-Parse-Application-Id": PARSE_APP_ID,
+		       "X-Parse-REST-API-Key": PARSE_APP_KEY
 		     })
 		result = json.loads(connection.getresponse().read())
 		print result
@@ -39,8 +41,8 @@ def hello():
 					if result["results"][0]["point_b"]:
 						connection.connect()
 						connection.request('DELETE', '/1/classes/Rider/%s' % result["results"][0]["objectId"], '', {
-							"X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-							"X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq"
+							"X-Parse-Application-Id": PARSE_APP_ID,
+							"X-Parse-REST-API-Key": PARSE_APP_KEY
 						})
 					else:
 						resp.message("Your route is ready! https://www.strava.com/athlete/routes")
@@ -50,8 +52,8 @@ def hello():
 						connection.request('PUT', '/1/classes/Rider/%s' % result["results"][0]["objectId"], json.dumps({
 							"point_b": body
 						}), {
-							"X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-							"X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq",
+							"X-Parse-Application-Id": PARSE_APP_ID,
+							"X-Parse-REST-API-Key": PARSE_APP_KEY,
 							"Content-Type": "application/json"
 						})
 
@@ -62,64 +64,23 @@ def hello():
 						     })})
 						connection.connect()
 						connection.request('GET', '/1/classes/Rider?%s' % params, '', {
-						       "X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-						       "X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq"
+						       "X-Parse-Application-Id": PARSE_APP_ID,
+						       "X-Parse-REST-API-Key": PARSE_APP_KEY
 						     })
 						result = json.loads(connection.getresponse().read())
 
-						# launch browser
-						# if selenium has firefox problems see this Stack Overflow question
-						# http://stackoverflow.com/questions/6682009/selenium-firefoxprofile-exception-cant-load-the-profile/25645344#25645344
-						display = Display(visible=0, size=(1440, 800))
-						display.start()
-						browser = webdriver.Firefox()
-
-						#log in
-						browser.get('http://www.strava.com/routes/new')
-						time.sleep(1)
-						email = browser.find_element_by_name("email")
-						email.send_keys(result["results"][0]["email"])
-						password = browser.find_element_by_name("password")
-						password.send_keys(result["results"][0]["password"])
-						loginButton = browser.find_element_by_id('login-button')
-						loginButton.click()
-						time.sleep(3)
-
-						#input first location
-						locationField = browser.find_element_by_css_selector('.input-lg')
-						locationField.send_keys(result["results"][0]["point_a"])
-						locationFieldButton = browser.find_element_by_css_selector('.icon')
-						locationFieldButton.click()
-						time.sleep(3)
-						canvas = browser.find_element_by_xpath('//*[@id="map-canvas"]/div/div[1]/div[2]')
-						canvas.click()
-						time.sleep(3)
-
-						#input second location
-						locationField.clear()
-						locationField.send_keys(result["results"][0]["point_b"])
-						locationFieldButton = browser.find_element_by_css_selector('.icon')
-						locationFieldButton.click()
-						time.sleep(3)
-						canvas = browser.find_element_by_xpath('//*[@id="map-canvas"]/div/div[1]/div[2]')
-						canvas.click()
-						time.sleep(3)
-
-						#save and name route
-						saveButton = browser.find_element_by_css_selector('.save-route')
-						saveButton.click()
-						routeName = browser.find_element_by_id('name')
-						routeName.send_keys(result["results"][0]["point_a"] + " to " + result["results"][0]["point_b"])
-						submitButton = browser.find_element_by_css_selector('.submit')
-						submitButton.click()
-
-						browser.quit()
-
-						display.stop()
-
+                        # Define the parameters
+                        user_email = result["results"][0]["email"]
+                        user_password = result["results"][0]["password"]
+                        loc1 = result["results"][0]["point_a"]
+                        loc2 = result["results"][0]["point_b"]
+                        
+                        # Create route in Strava
+                        create_route(user_email, user_password, loc1, loc2)
+                        
 						connection.request('DELETE', '/1/classes/Rider/%s' % result["results"][0]["objectId"], '', {
-							"X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-							"X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq"
+							"X-Parse-Application-Id": PARSE_APP_ID,
+							"X-Parse-REST-API-Key": PARSE_APP_KEY
 						})
 
 						return str(resp)
@@ -130,8 +91,8 @@ def hello():
 					connection.request('PUT', '/1/classes/Rider/%s' % result["results"][0]["objectId"], json.dumps({
 						"point_a": body
 					}), {
-						"X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-						"X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq",
+						"X-Parse-Application-Id": PARSE_APP_ID,
+						"X-Parse-REST-API-Key": PARSE_APP_KEY,
 						"Content-Type": "application/json"
 					})
 					resp.message("What's your ending location?")
@@ -141,8 +102,8 @@ def hello():
 				connection.request('PUT', '/1/classes/Rider/%s' % result["results"][0]["objectId"], json.dumps({
 					"password": body
 				}), {
-					"X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-					"X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq",
+					"X-Parse-Application-Id": PARSE_APP_ID,
+					"X-Parse-REST-API-Key": PARSE_APP_KEY,
 					"Content-Type": "application/json"
 				})
 				resp.message("What's your starting location?")
@@ -152,8 +113,8 @@ def hello():
 			connection.request('PUT', '/1/classes/Rider/%s' % result["results"][0]["objectId"], json.dumps({
 				"email": body
 			}), {
-				"X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-				"X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq",
+				"X-Parse-Application-Id": PARSE_APP_ID,
+				"X-Parse-REST-API-Key": PARSE_APP_KEY,
 				"Content-Type": "application/json"
 			})
 			resp.message("What's your password?")
@@ -167,8 +128,8 @@ def hello():
 			"point_a": '',
 			"point_b": ''
 		}), {
-			"X-Parse-Application-Id": "AOJncxqz885qqhXNcjrvgWrozTAAXPoMwezKue1K",
-			"X-Parse-REST-API-Key": "HTwOFGukRZClAFrQezSKMDtcYhKtsL8alF64EFdq",
+			"X-Parse-Application-Id": PARSE_APP_ID,
+			"X-Parse-REST-API-Key": PARSE_APP_KEY,
 			"Content-Type": "application/json"
 		})
 		resp.message("What's your email?")
---
+++ strava.py
@@ -0,0 +1,85 @@
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+import selenium.webdriver.support.expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.by import By
+from pyvirtualdisplay import Display
+from selenium import webdriver
+import logging
+import time
+
+
+class Strava(object):
+
+    def __init__(self, headless=True):
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        self.headless = headless
+        if self.headless:
+            self.display = Display(size=(1440, 800))
+            self.display.start()
+        profile = FirefoxProfile()
+        #profile.set_preference('permissions.default.image', 2)
+        self.browser = webdriver.Firefox(firefox_profile=profile)
+
+    def login(self, user_email, user_password):
+        self.logger.info('Attempting to login...')
+        self.browser.get('http://www.strava.com/routes/new')
+        time.sleep(1)
+        email = self.browser.find_element_by_name("email")
+        email.send_keys(user_email)
+        password = self.browser.find_element_by_name("password")
+        password.send_keys(user_password)
+        loginButton = self.browser.find_element_by_id('login-button')
+        loginButton.submit()
+        self.logger.info('Login successful!')
+
+    def input_location(self, loc):
+        self.logger.info('Registering location...')
+        self.locationField = self.browser.find_element_by_css_selector('.input-lg')
+        self.locationField.clear()
+        self.locationField.send_keys(loc)
+        self.locationFieldButton = self.browser.find_element_by_css_selector('.icon')
+        self.locationFieldButton.click()
+        time.sleep(3)
+        canvas = self.browser.find_element_by_xpath('//*[@id="map-canvas"]/div/div[1]/div[2]')
+        canvas.click()
+        time.sleep(3)
+
+    def save_route(self, loc1, loc2):
+        self.logger.info('Saving route...')
+        saveButton = self.browser.find_element_by_css_selector('.save-route')
+        saveButton.click()
+        routeName = self.browser.find_element_by_id('name')
+        routeName.send_keys(loc1 + " to " + loc2)
+        submitButton = self.browser.find_element_by_css_selector('.submit')
+        submitButton.click()
+        # Wait for the success message to appear
+        try:
+            saved_selector = '//h3[text()="Your Route has been saved"]'
+            test_elem = EC.visibility_of_element_located((By.XPATH, saved_selector))
+            wait = ui.WebDriverWait(self.browser, 20).until(test_elem)
+            self.logger.info('Route successfully saved!')
+        except TimeoutException:
+            pass
+    
+    def close(self):
+        self.browser.quit()
+        if self.headless:
+            self.display.stop()
+            
+
+def strava_interface(email,password, loc1, loc2):
+    s = Strava()
+    s.login(email, password)
+    s.input_location(loc1)
+    s.input_location(loc2)
+    s.save_route(loc1, loc2)
+    s.close()
+    
+
+if __name__ == '__main__':
+    import sys
+    email, password, loc1, loc2 = sys.argv[1:]
+    strava_interface(email, password, loc1, loc2)
+
